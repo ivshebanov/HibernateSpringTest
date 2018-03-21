c74407877358d821727861fa96292bd0f043529b
Ilya Shebanov <ivshebanov@gmail.com>
2018-03-21 16:08:50 +0300

    modification BD

--- eas/src/main/resources/data.sql
+++ eas/src/main/resources/data.sql
@@ -1,51 +1,50 @@
-INSERT INTO Docs (code, version, doc_name) VALUES (1, 0, 'Свидетельство о рождении');
-INSERT INTO Docs (code, version, doc_name) VALUES (2, 0, 'Военный билет');
-INSERT INTO Docs (code, version, doc_name) VALUES (3, 0, 'Временное удостоверение, выданное взамен военного билета');
-INSERT INTO Docs (code, version, doc_name) VALUES (4, 0, 'Паспорт иностранного гражданина');
-INSERT INTO Docs (code, version, doc_name) VALUES (5, 0, 'Вид на жительство в Российской Федерации');
-INSERT INTO Docs (code, version, doc_name) VALUES (6, 0, 'Удостоверение беженца');
-INSERT INTO Docs (code, version, doc_name) VALUES (7, 0, 'Разрешение на временное проживание в Российской Федерации');
-INSERT INTO Docs (code, version, doc_name) VALUES (8, 0, 'Свидетельство о предоставлении убежища на территории РФ');
-INSERT INTO Docs (code, version, doc_name) VALUES (9, 0, 'Паспорт гражданина Российской Федерации');
-INSERT INTO Docs (code, version, doc_name) VALUES (10, 0, 'Свидетельство о рождении, выданное органом иностранного гос');
-INSERT INTO Docs (code, version, doc_name) VALUES (11, 0, 'Удостоверение личности военнослужащего Российской Федерации');
+INSERT INTO Doc_type (code, version, doc_name) VALUES (1, 0, 'Свидетельство о рождении');
+INSERT INTO Doc_type (code, version, doc_name) VALUES (2, 0, 'Военный билет');
+INSERT INTO Doc_type (code, version, doc_name)
+VALUES (3, 0, 'Временное удостоверение, выданное взамен военного билета');
+INSERT INTO Doc_type (code, version, doc_name) VALUES (4, 0, 'Паспорт иностранного гражданина');
+INSERT INTO Doc_type (code, version, doc_name) VALUES (5, 0, 'Вид на жительство в Российской Федерации');
+INSERT INTO Doc_type (code, version, doc_name) VALUES (6, 0, 'Удостоверение беженца');
+INSERT INTO Doc_type (code, version, doc_name)
+VALUES (7, 0, 'Разрешение на временное проживание в Российской Федерации');
+INSERT INTO Doc_type (code, version, doc_name) VALUES (8, 0, 'Свидетельство о предоставлении убежища на территории РФ');
+INSERT INTO Doc_type (code, version, doc_name) VALUES (9, 0, 'Паспорт гражданина Российской Федерации');
+INSERT INTO Doc_type (code, version, doc_name)
+VALUES (10, 0, 'Свидетельство о рождении, выданное органом иностранного гос');
+INSERT INTO Doc_type (code, version, doc_name)
+VALUES (11, 0, 'Удостоверение личности военнослужащего Российской Федерации');
 
-INSERT INTO Countries (code, version, citizenship_name) VALUES (1, 0, 'Австрия');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (2, 0, 'Албания');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (3, 0, 'Андорра');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (4, 0, 'Белоруссия');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (5, 0, 'Бельгия');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (6, 0, 'Болгария');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (7, 0, 'Босния и Герцеговина');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (8, 0, 'Ватикан');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (9, 0, 'Польша');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (10, 0, 'Российская Федерация');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (11, 0, 'Чехия');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (12, 0, 'Швейцария');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (13, 0, 'Швеция');
-INSERT INTO Countries (code, version, citizenship_name) VALUES (14, 0, 'Эстония');
+INSERT INTO Country (code, version, citizenship_name) VALUES (1, 0, 'Австрия');
+INSERT INTO Country (code, version, citizenship_name) VALUES (2, 0, 'Албания');
+INSERT INTO Country (code, version, citizenship_name) VALUES (3, 0, 'Андорра');
+INSERT INTO Country (code, version, citizenship_name) VALUES (4, 0, 'Белоруссия');
+INSERT INTO Country (code, version, citizenship_name) VALUES (5, 0, 'Бельгия');
+INSERT INTO Country (code, version, citizenship_name) VALUES (6, 0, 'Болгария');
+INSERT INTO Country (code, version, citizenship_name) VALUES (7, 0, 'Босния и Герцеговина');
+INSERT INTO Country (code, version, citizenship_name) VALUES (8, 0, 'Ватикан');
+INSERT INTO Country (code, version, citizenship_name) VALUES (9, 0, 'Польша');
+INSERT INTO Country (code, version, citizenship_name) VALUES (10, 0, 'Российская Федерация');
+INSERT INTO Country (code, version, citizenship_name) VALUES (11, 0, 'Чехия');
+INSERT INTO Country (code, version, citizenship_name) VALUES (12, 0, 'Швейцария');
+INSERT INTO Country (code, version, citizenship_name) VALUES (13, 0, 'Швеция');
+INSERT INTO Country (code, version, citizenship_name) VALUES (14, 0, 'Эстония');
 
-INSERT INTO Register (name, version, login, password, hash_active)
-VALUES ('bell', 0, 'Shebanov', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', '12345hashcode');
-INSERT INTO Register (name, version, login, password, hash_active)
-VALUES ('Сбертех', 0, 'сбер', 'e0bc60c82713f64ef8a57c0c40d02ce24fd0141d5cc3086259c19b1e62a62bea', '112233hashcode');
-
-INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active)
-VALUES (0, 'bell', 'bellintegrator', 1234567890, 0987654321, 'Москва, ул. Арбат, д.1', 567898, 1);
-INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active)
-VALUES (0, 'Сбертех', 'СбербанкТехнологии', 11223344, 66778899, 'Москва, ул. Остоженка, д.2', 876678, 0);
+INSERT INTO Organization (version, name, full_name, login, password, inn, kpp, address, phone, is_active, hash_active)
+VALUES (0, 'bell', 'bellintegrator', 'Shebanov', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5',
+           1234567890, 0987654321, 'Москва, ул. Арбат, д.1', 567898, 1, '12345hashcode');
+INSERT INTO Organization (version, name, full_name, login, password, inn, kpp, address, phone, is_active, hash_active)
+VALUES (0, 'Сбертех', 'СбербанкТехнологии', 'сбер', 'e0bc60c82713f64ef8a57c0c40d02ce24fd0141d5cc3086259c19b1e62a62bea',
+           11223344, 66778899, 'Москва, ул. Остоженка, д.2', 876678, 0, '112233hashcode');
 
 INSERT INTO Office (version, name, address, phone, is_active, org_id)
 VALUES (0, 'belloffice', 'Москва, ул. Арбат, д.1', 567898, 1, 1);
 INSERT INTO Office (version, name, address, phone, is_active, org_id)
 VALUES (0, 'Сбертехофис', 'Москва, ул. Остоженка, д.2', 876678, 1, 2);
 
-INSERT INTO User (version, first_name, second_name, middle_name, position, phone, office_id)
-VALUES (0, 'Сергей', 'Сидоров', 'Викторович', 'Руководитель отдела', 556677, 1);
-INSERT INTO User (version, first_name, second_name, middle_name, position, phone, office_id)
-VALUES (0, 'Иван', 'Инванов', 'Иванович', 'Руководитель отдела кадров', 998877, 2);
-
-INSERT INTO Documentation (version, user_id, doc_code, doc_number, doc_date, country_code, is_identified)
-VALUES (0, 1, 5, 151, '2004-10-21', 1, 1);
-INSERT INTO Documentation (version, user_id, doc_code, doc_number, doc_date, country_code, is_identified)
-VALUES (0, 2, 1, 678, '2018-11-11', 5, 1);
+INSERT INTO User (version, first_name, second_name, middle_name, position, phone, doc_code, doc_name, doc_number, doc_date, citizenship_code, citizenship_name, is_identified, office_id)
+VALUES
+  (0, 'Сергей', 'Сидоров', 'Викторович', 'Руководитель отдела', 556677, 5, 'Вид на жительство в Российской Федерации',
+      151, '2004-10-21', 1, 'Австрия', 1, 1);
+INSERT INTO User (version, first_name, second_name, middle_name, position, phone, doc_code, doc_name, doc_number, doc_date, citizenship_code, citizenship_name, is_identified, office_id)
+VALUES (0, 'Иван', 'Инванов', 'Иванович', 'Руководитель отдела кадров', 998877, 1, 'Свидетельство о рождении', 678,
+           '2018-11-11', 8, 'Ватикан', 1, 2);
--- eas/src/main/resources/schema.sql
+++ eas/src/main/resources/schema.sql
@@ -1,22 +1,16 @@
-CREATE TABLE IF NOT EXISTS Register (
+CREATE TABLE IF NOT EXISTS Organization (
   id          INTEGER PRIMARY KEY AUTO_INCREMENT,
-  name        VARCHAR(50) NOT NULL,
-  version     INTEGER     NOT NULL,
+  version     INTEGER      NOT NULL,
+  name        VARCHAR(50)  NOT NULL,
+  full_name   VARCHAR(50)  NOT NULL,
   login       VARCHAR(100) NOT NULL,
   password    VARCHAR(100) NOT NULL,
+  inn         INTEGER      NOT NULL,
+  kpp         INTEGER      NOT NULL,
+  address     VARCHAR(50)  NOT NULL,
+  phone       INTEGER      NOT NULL,
+  is_active   BIT          NOT NULL,
   hash_active VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Organization (
-  id        INTEGER PRIMARY KEY AUTO_INCREMENT,
-  version   INTEGER     NOT NULL,
-  name      VARCHAR(50) NOT NULL,
-  full_name VARCHAR(50) NOT NULL,
-  inn       INTEGER     NOT NULL,
-  kpp       INTEGER     NOT NULL,
-  address   VARCHAR(50) NOT NULL,
-  phone     INTEGER     NOT NULL,
-  is_active BIT         NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Office (
@@ -30,59 +24,43 @@ CREATE TABLE IF NOT EXISTS Office (
 );
 
 CREATE TABLE IF NOT EXISTS User (
-  id          INTEGER PRIMARY KEY AUTO_INCREMENT,
-  version     INTEGER     NOT NULL,
-  first_name  VARCHAR(50) NOT NULL,
-  second_name VARCHAR(50) NOT NULL,
-  middle_name VARCHAR(50) NOT NULL,
-  position    VARCHAR(50) NOT NULL,
-  phone       INTEGER     NOT NULL,
-  office_id   INTEGER
+  id               INTEGER PRIMARY KEY AUTO_INCREMENT,
+  version          INTEGER     NOT NULL,
+  first_name       VARCHAR(50) NOT NULL,
+  second_name      VARCHAR(50) NOT NULL,
+  middle_name      VARCHAR(50) NOT NULL,
+  position         VARCHAR(50) NOT NULL,
+  phone            INTEGER     NOT NULL,
+  doc_code         INTEGER     NOT NULL,
+  doc_name         VARCHAR(60) NOT NULL,
+  doc_number       INTEGER     NOT NULL,
+  doc_date         DATE        NOT NULL,
+  citizenship_code INTEGER     NOT NULL,
+  citizenship_name VARCHAR(50) NOT NULL,
+  is_identified    BIT         NOT NULL,
+  office_id        INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS Documentation (
-  id            INTEGER PRIMARY KEY AUTO_INCREMENT,
-  version       INTEGER NOT NULL,
-  user_id       INTEGER,
-  doc_code      INTEGER,
-  doc_number    INTEGER NOT NULL,
-  doc_date      DATE    NOT NULL,
-  country_code  INTEGER,
-  is_identified BIT     NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Docs (
+CREATE TABLE IF NOT EXISTS Doc_type (
   id       INTEGER PRIMARY KEY AUTO_INCREMENT,
   code     INTEGER     NOT NULL,
   version  INTEGER     NOT NULL,
   doc_name VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Countries (
+CREATE TABLE IF NOT EXISTS Country (
   id               INTEGER PRIMARY KEY AUTO_INCREMENT,
   code             INTEGER     NOT NULL,
   version          INTEGER     NOT NULL,
   citizenship_name VARCHAR(50) NOT NULL
 );
 
--- ALTER TABLE Organization
---   ADD FOREIGN KEY (name) REFERENCES Register (name);
 ALTER TABLE Office
   ADD FOREIGN KEY (org_id) REFERENCES Organization (id);
 ALTER TABLE User
   ADD FOREIGN KEY (office_id) REFERENCES Office (id);
-ALTER TABLE Documentation
-  ADD FOREIGN KEY (user_id) REFERENCES User (id);
-ALTER TABLE Documentation
-  ADD FOREIGN KEY (doc_code) REFERENCES Docs (code);
-ALTER TABLE Documentation
-  ADD FOREIGN KEY (country_code) REFERENCES Countries (code);
 
-CREATE INDEX IX_Organization_name
-  ON Organization (name);
 CREATE INDEX IX_Office_org_id
   ON Office (org_id);
 CREATE INDEX IX_User_office_id
   ON User (office_id);
-CREATE INDEX IX_Documentation_user_id_doc_code_country_code
-  ON Documentation (user_id, doc_code, country_code);
