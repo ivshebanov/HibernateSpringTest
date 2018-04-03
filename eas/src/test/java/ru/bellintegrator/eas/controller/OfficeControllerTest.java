c1b636604b3769818e3bbf76f5b36f63db7ec683
Ilya Shebanov <ivshebanov@gmail.com>
2018-04-03 23:04:59 +0300

    Tests

--- eas/src/test/java/ru/bellintegrator/eas/controller/OfficeControllerTest.java
+++ eas/src/test/java/ru/bellintegrator/eas/controller/OfficeControllerTest.java
@@ -1,24 +1,32 @@
 package ru.bellintegrator.eas.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
-import ru.bellintegrator.eas.Application;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.bellintegrator.eas.service.OfficeService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class})
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration(value = "src/main/resources")
-@Transactional
-@DirtiesContext
 public class OfficeControllerTest {
 
+    private OfficeService officeServiceMock;
+
     @Autowired
-    private OfficeController controller;
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
 
     @Test
     public void loadOfficeTest() {
@@ -31,7 +39,7 @@ public class OfficeControllerTest {
     }
 
     @Test
-    public void updateTest()  {
+    public void updateTest() {
 
     }
 
--- eas/src/test/java/ru/bellintegrator/eas/controller/UserControllerTest.java
+++ eas/src/test/java/ru/bellintegrator/eas/controller/UserControllerTest.java
@@ -1,20 +1,28 @@
 package ru.bellintegrator.eas.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
-import ru.bellintegrator.eas.Application;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.bellintegrator.eas.service.UserService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class})
-@WebAppConfiguration(value = "src/main/resources")
-@Transactional
-@DirtiesContext
 public class UserControllerTest {
+
+    private UserService userService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     public void loadUserTest() {
